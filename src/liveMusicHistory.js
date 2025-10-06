17dbdabd5a547c34fd15091995aa3208f9ac5b54
Kaiano Levine <kaiano.levine@gmail.com>
2025-10-06 18:27:11 -0500

    Minor UPdates to try

--- src/liveMusicHistory.js
+++ src/liveMusicHistory.js
@@ -1,8 +1,9 @@
+const FOLDER_ID = '1FzuuO3xmL2n-8pZ_B-FyrvGWaLxLED3o'; // Google Drive folder ID to monitor
+const SNAPSHOT_PROPERTY_NAME = 'vdjHistorySnapshot'; //Unique Snapshot Property Name
+const REPO_OWNER = 'kaianolevine'; //GitHub Repo Owner
+const REPO_NAME = 'combined-tools-sandbox'; //Github Repo Name
+const EVENT_TYPE = 'vdj_history'; //event_type for GitHub 'repository_dispatch'
+
 function vdjMusicHistory() {
-    monitorDriveFolderAndTriggerGitHub(
-        '1FzuuO3xmL2n-8pZ_B-FyrvGWaLxLED3o', //FOLDER_ID
-        'vdjHistorySnapshot', //Unique Snapshot Property Name
-        'kaianolevine', //GitHub Repo Owner
-        'combined-tools-sandbox', //Github Repo Name
-        'vdj_history'); //event_type for GitHub 'repository_dispatch'
+    monitorDriveFolderAndTriggerGitHub(FOLDER_ID, SNAPSHOT_PROPERTY_NAME, REPO_OWNER, REPO_NAME, EVENT_TYPE); 
 }
--- src/westieDjTools.js
+++ src/westieDjTools.js
@@ -1,4 +1,4 @@
-const FOLDER_ID = ''; // Google Drive folder ID to monitor
-const REPO_OWNER = 'kaianolevine';
-const REPO_NAME = 'westie-dj-tools';
-const EVENT_TYPE = 'drive-change';
+//const FOLDER_ID = ''; // Google Drive folder ID to monitor
+//const REPO_OWNER = 'kaianolevine';
+//const REPO_NAME = 'westie-dj-tools';
+//const EVENT_TYPE = 'drive-change';
