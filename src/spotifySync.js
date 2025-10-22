eee6b170ca84e4edf05e5c8ddebf01a4699819ac
Kaiano Levine <kaiano.levine@gmail.com.com>
2025-10-22 16:22:17 -0500

    fixing trigger

--- src/spotifySync.js
+++ src/spotifySync.js
@@ -1,4 +1,5 @@
 function spotifySync() {
+    const REPO_OWNER = 'kaianolevine'; //GitHub Repo Owner
     const REPO_NAME = 'spotify-playlist-generator-dev'; //Github Repo Name
     const EVENT_TYPE = 'spotify_sync'; //event_type for GitHub 'repository_dispatch'
     triggerGitHubAction(REPO_OWNER, REPO_NAME, EVENT_TYPE);
