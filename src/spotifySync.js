4339f28c76c83b89ae12e1ad9aa164ffd4021e34
Kaiano Levine <kaiano.levine@gmail.com.com>
2025-10-22 16:15:45 -0500

    Connecting to new process

--- src/spotifySync.js
+++ src/spotifySync.js
@@ -1,5 +1,5 @@
 function spotifySync() {
-    const REPO_NAME = 'combined-tools-sandbox'; //Github Repo Name
+    const REPO_NAME = 'spotify-playlist-generator-dev'; //Github Repo Name
     const EVENT_TYPE = 'spotify_sync'; //event_type for GitHub 'repository_dispatch'
     triggerGitHubAction(REPO_OWNER, REPO_NAME, EVENT_TYPE);
 }
