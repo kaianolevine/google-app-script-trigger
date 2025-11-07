d6cc917d72a8dfd6f5f208e575aff4809791d978
Kaiano Levine <kaiano.levine@gmail.com.com>
2025-11-07 14:54:48 -0600

    Update repo running code

--- src/generateSummary.js
+++ src/generateSummary.js
@@ -2,7 +2,7 @@ function generateNewSummary() {
     const FOLDER_ID = '1A0tKQ2DBXI1Bt9h--olFwnBNne3am-rL'; // Google Drive folder ID to monitor
     const SNAPSHOT_PROPERTY_NAME = 'summarySnapshot'; //Unique Snapshot Property Name
     const REPO_OWNER = 'kaianolevine'; //GitHub Repo Owner
-    const REPO_NAME = 'combined-tools-sandbox'; //Github Repo Name
+    const REPO_NAME = 'deejay-set-processor-dev'; //Github Repo Name
     const EVENT_TYPE = 'generate-summary'; //event_type for GitHub 'repository_dispatch'
     const EXCLUDED_FILES = [ // List of files to exclude (can be by name or by ID)
         ""
