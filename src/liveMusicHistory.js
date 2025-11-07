be77fe988bfad284de229fff8d865e4af2861b82
Kaiano Levine <kaiano.levine@gmail.com.com>
2025-11-07 16:27:13 -0600

    Updating to new repo

--- src/liveMusicHistory.js
+++ src/liveMusicHistory.js
@@ -2,7 +2,7 @@ function vdjMusicHistory() {
     const FOLDER_ID = '1FzuuO3xmL2n-8pZ_B-FyrvGWaLxLED3o'; // Google Drive folder ID to monitor
     const SNAPSHOT_PROPERTY_NAME = 'vdjHistorySnapshot'; //Unique Snapshot Property Name
     const REPO_OWNER = 'kaianolevine'; //GitHub Repo Owner
-    const REPO_NAME = 'combined-tools-sandbox'; //Github Repo Name
+    const REPO_NAME = 'live-music-history-dev'; //Github Repo Name
     const EVENT_TYPE = 'vdj_history'; //event_type for GitHub 'repository_dispatch'
     const EXCLUDED_FILES = [ // List of files to exclude (can be by name or by ID)
         ""
