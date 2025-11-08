ddf4b904640ba4a87f96a1180e0df3a3cdce37e2
kaianolevine <kaiano.levine@gmail.com>
2025-11-08 11:49:06 -0600

    Change event type for GitHub repository dispatch

--- src/spotifySync.js
+++ src/spotifySync.js
@@ -1,6 +1,6 @@
 function spotifySync() {
     const REPO_OWNER = 'kaianolevine'; //GitHub Repo Owner
     const REPO_NAME = 'spotify-playlist-generator-dev'; //Github Repo Name
-    const EVENT_TYPE = 'spotify_sync'; //event_type for GitHub 'repository_dispatch'
+    const EVENT_TYPE = 'GoogleAppScript_SpotifySync'; //event_type for GitHub 'repository_dispatch'
     triggerGitHubAction(REPO_OWNER, REPO_NAME, EVENT_TYPE);
 }
